9a0142dfa4ccbf487c9d04cd2f327414c21d8e93
Júlia Ferreira <jfo3@aluno.ifal.edu.br>
2022-08-04 17:05:08 -0300

    feat: Criação do botão

---
+++ src/components/buttons/SecondaryButton/index.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import styles from './styles.module.css';
+
+export default function SecondaryButton({ title, onClick, Icon }) {
+  return (
+    <button className={styles.container} type="button" onClick={onClick}>
+      {title}
+      {Icon && <Icon size={20} strokeWidth={2.5} />}
+    </button>
+  );
+}
+
+SecondaryButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  Icon: PropTypes.func.isRequired,
+};
