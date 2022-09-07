b85b1856dc2941989c678856b91448fdb437a95c
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-09-07 13:33:38 -0300

    feat(pageContentController): add getPageContent function

---
+++ src/controllers/pageContentController.js
@@ -0,0 +1,25 @@
+import getDocuments from '../services/firebase/firestore';
+
+export default async function getPageContent(pageId) {
+  try {
+    const result = await getDocuments('texts');
+    let pageContent = {};
+
+    if (result) {
+      result.docs.forEach((snapshot) => {
+        if (snapshot.id === pageId) {
+          pageContent = {
+            id: snapshot.id,
+            ...snapshot.data(),
+          };
+        }
+      });
+    }
+
+    return pageContent;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return null;
+  }
+}
