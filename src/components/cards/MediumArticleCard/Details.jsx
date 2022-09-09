104f3d4ee8698493259d21076d7dd22f78b26a06
Júlia Ferreira <jfo3@aluno.ifal.edu.br>
2022-09-09 00:10:44 -0300

    feat: Criação do card de artigos

---
+++ src/components/cards/MediumArticleCard/Details.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Details.module.css';
+
+export default function Details({ date, tags }) {
+  function getFormatedTags() {
+    const formatedTags = (tags.map((tag) => (`#${tag}`)).join(' '));
+    return formatedTags;
+  }
+  return (
+    <div className={styles.container}>
+      <p className={styles.details}>{date}</p>
+      <span className={styles.divider}> | </span>
+      <p className={styles.details}>{getFormatedTags()}</p>
+    </div>
+  );
+}
+
+Details.propTypes = {
+  date: PropTypes.string.isRequired,
+  tags: PropTypes.array.isRequired,
+};
