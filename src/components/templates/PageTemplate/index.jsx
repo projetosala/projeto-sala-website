292793f875ff2a8dcba32d44e782ad241240a5d3
mayarinhan <ms8@aluno.ifal.edu.br>
2022-09-17 11:57:02 -0300

    feat: criação do botão circular

--- src/components/templates/PageTemplate/index.jsx
+++ src/components/templates/PageTemplate/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../Header';
 import Footer from '../../Footer';
+import FloatingButton from '../../buttons/FloatingButton';
 
 export default function PageTemplate({ title, description }) {
   return (
     <>
       <Header title={title} description={description} />
       <Footer />
+      <FloatingButton />
     </>
   );
 }
