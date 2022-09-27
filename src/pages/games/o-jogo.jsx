b5d07e9ba0613f47da2d86a03fa21848354b054a
Dheverton Pereira <dhevv04@gmail.com>
2022-09-27 13:52:19 -0300

    refactor: Rever rotas da aplicação

--- src/pages/games/o-jogo.jsx
+++ src/pages/games/o-jogo.jsx
@@ -1,9 +1,9 @@
-import PageTemplate from '../components/templates/PageTemplate';
+import PageTemplate from '../../components/templates/PageTemplate';
 
 export default function TheGame() {
   return (
     <PageTemplate
-      title="O Jogo"
+      title="Mutum: Desafio Ambiental"
       description="Conheça as regras, personagens, cartas e o que inspirou a criação do jogo"
     />
   );
