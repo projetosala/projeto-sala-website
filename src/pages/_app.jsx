5070d6d53e436977fec000d2bb1baf1e32aeb11e
Dheverton Pereira <dhevv04@gmail.com>
2022-09-30 17:50:45 -0300

    feat: Adição dos titles e das metatags

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Uma abordagem lúdica sobre a importância da preservação do meio ambiente alagoano" />
         <title>Projeto S.A.L.A</title>
       </Head>
 
