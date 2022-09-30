fc327b9d93af84af4364bcd4c69a485764ee55e4
Dheverton Pereira <dhevv04@gmail.com>
2022-09-30 17:50:45 -0300

    feat: Adição dos titles e das metatags

--- src/pages/artigos.jsx
+++ src/pages/artigos.jsx
@@ -1,10 +1,20 @@
+import Head from 'next/head';
 import PageTemplate from '../components/templates/PageTemplate';
 
 export default function Articles() {
   return (
-    <PageTemplate
-      title="Artigos"
-      description="Todos os artigos científicos e postagens no Medium que publicamos até o momento"
-    />
+    <>
+      <Head>
+        <meta name="keywords" content="analise ambiental, geografia, jogos" />
+        <meta name="description" content="Todos os artigos científicos e postagens que publicamos até o momento" />
+        <title>Artigos</title>
+      </Head>
+
+      <PageTemplate
+        title="Artigos"
+        description="Todos os artigos científicos e postagens que publicamos até o momento"
+      />
+
+    </>
   );
 }
