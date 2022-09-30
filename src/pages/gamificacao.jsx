c06fbb0b8587efe6cc1c6a675b3fef76c9ca4127
Dheverton Pereira <dhevv04@gmail.com>
2022-09-30 17:50:45 -0300

    feat: Adição dos titles e das metatags

--- src/pages/gamificacao.jsx
+++ src/pages/gamificacao.jsx
@@ -1,10 +1,19 @@
+import Head from 'next/head';
 import PageTemplate from '../components/templates/PageTemplate';
 
 export default function Gamification() {
   return (
-    <PageTemplate
-      title="Gamificação"
-      description="Você quer aprender ou se divertir? E que tal os dois?"
-    />
+    <>
+      <Head>
+        <meta name="keywords" content="realidade aumentada, RPG, tabuleiro" />
+        <meta name="description" content="Você quer aprender ou se divertir? E que tal os dois?" />
+        <title>Gamificação</title>
+      </Head>
+
+      <PageTemplate
+        title="Gamificação"
+        description="Você quer aprender ou se divertir? E que tal os dois?"
+      />
+    </>
   );
 }
--- src/pages/o-projeto.jsx
+++ src/pages/o-projeto.jsx
@@ -1,10 +1,20 @@
+import Head from 'next/head';
 import PageTemplate from '../components/templates/PageTemplate';
 
 export default function TheProject() {
   return (
-    <PageTemplate
-      title="O Projeto"
-      description="Um pouco do que inspirou esta iniciativa, quem nós somos e o que conseguimos construir"
-    />
+    <>
+      <Head>
+        <meta name="keywords" content="gamificação, alagoas, biomas" />
+        <meta name="description" content="Um pouco do que inspirou esta iniciativa, quem nós somos e o que conseguimos construir" />
+        <title>O Projeto</title>
+      </Head>
+
+      <PageTemplate
+        title="O Projeto"
+        description="Um pouco do que inspirou esta iniciativa, quem nós somos e o que conseguimos construir"
+      />
+    </>
+
   );
 }
