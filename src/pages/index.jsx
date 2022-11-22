29e8c2584aec175c8cba1d5c3f95251a12ba5a45
Júlia Ferreira <jfo3@aluno.ifal.edu.br>
2022-11-22 08:36:00 -0300

    feat: Criação do Carrossel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,47 +2,60 @@ import PropTypes from 'prop-types';
 import CtaSection from '../components/base/CtaSection';
 import PageTemplate from '../components/templates/PageTemplate';
 import getPageContent from '../controllers/pageContentController';
+import Carousel from '../components/gallery/Carousel';
+import getImages from '../controllers/galleryController';
 
-export default function Index({ texts }) {
+export default function Index({ images, texts }) {
   return (
     <PageTemplate
       title="Projeto S.A.L.A"
       description="Uma abordagem lúdica sobre a importância da preservação do meio ambiente alagoano"
     >
-      <PageContent texts={texts} />
+      <PageContent images={images} texts={texts} />
     </PageTemplate>
   );
 }
 
-function PageContent({ texts }) {
-  if (!texts) {
+function PageContent({ images, texts }) {
+  if (!texts || !images) {
     window.location.reload();
     return null;
   }
 
   return (
-    <CtaSection
-      title="S.A.L.A"
-      text={texts.intro}
-      actionText="Conheça o projeto"
-      target="/o-projeto"
-    />
+    <>
+      <CtaSection
+        title="S.A.L.A"
+        text={texts.intro}
+        actionText="Conheça o projeto"
+        target="/o-projeto"
+      />
+      <Carousel images={images} />
+    </>
   );
 }
 
 export async function getStaticProps() {
   const now = 0;
   const oneWeekInSeconds = 604800;
-  const texts = await getPageContent('home');
-  const whenRevalidate = texts ? oneWeekInSeconds : now;
+  let texts = null;
+  let images = null;
 
-  return { props: { texts }, revalidate: whenRevalidate };
+  await Promise.all([getPageContent('home'), getImages()]).then((values) => {
+    [texts, images] = values;
+  });
+
+  const whenRevalidate = (texts && images) ? oneWeekInSeconds : now;
+
+  return { props: { images, texts }, revalidate: whenRevalidate };
 }
 
 Index.propTypes = {
   texts: PropTypes.object.isRequired,
+  images: PropTypes.array.isRequired,
 };
 
 PageContent.propTypes = {
   texts: PropTypes.object.isRequired,
+  images: PropTypes.array.isRequired,
 };
