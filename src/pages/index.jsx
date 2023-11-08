3704d0389f93617000689d0c9b9ab9e3222ec93b
dhianapereira <dhianaslv@outlook.com>
2023-11-08 16:28:44 -0300

    refactor: O conteúdo da home agora é local

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,61 +1,25 @@
-import PropTypes from 'prop-types';
 import CtaSection from '../components/base/CtaSection';
 import PageTemplate from '../components/templates/PageTemplate';
-import getPageContent from '../controllers/pageContentController';
 import Carousel from '../components/gallery/Carousel';
-import getImages from '../controllers/galleryController';
 
-export default function Index({ images, texts }) {
+import homeContent from '../data/home_page_content.json';
+import galleryContent from '../data/gallery_content.json';
+
+export default function Index() {
   return (
     <PageTemplate
       title="Projeto S.A.L.A"
       description="Uma abordagem lúdica sobre a importância da preservação do meio ambiente alagoano"
     >
-      <PageContent images={images} texts={texts} />
+      <>
+        <CtaSection
+          title="S.A.L.A"
+          text={homeContent.intro}
+          actionText="Conheça o projeto"
+          target="/o-projeto"
+        />
+        <Carousel images={galleryContent.images} />
+      </>
     </PageTemplate>
   );
 }
-
-function PageContent({ images, texts }) {
-  if (!texts || !images) {
-    window.location.reload();
-    return null;
-  }
-
-  return (
-    <>
-      <CtaSection
-        title="S.A.L.A"
-        text={texts.intro}
-        actionText="Conheça o projeto"
-        target="/o-projeto"
-      />
-      <Carousel images={images} />
-    </>
-  );
-}
-
-export async function getStaticProps() {
-  const now = 1;
-  const oneWeekInSeconds = 604800;
-  let texts = null;
-  let images = null;
-
-  await Promise.all([getPageContent('home'), getImages()]).then((values) => {
-    [texts, images] = values;
-  });
-
-  const whenRevalidate = (texts && images) ? oneWeekInSeconds : now;
-
-  return { props: { images, texts }, revalidate: whenRevalidate };
-}
-
-Index.propTypes = {
-  texts: PropTypes.object.isRequired,
-  images: PropTypes.array.isRequired,
-};
-
-PageContent.propTypes = {
-  texts: PropTypes.object.isRequired,
-  images: PropTypes.array.isRequired,
-};
