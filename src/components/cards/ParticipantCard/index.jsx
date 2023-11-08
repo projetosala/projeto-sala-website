65f036d80b96e1302a19c9f1a87d1bb1cd83f2f4
dhianapereira <dhianaslv@outlook.com>
2023-11-08 20:32:03 -0300

    feat: Adiciona a seção de participantes do projeto

--- src/components/cards/ParticipantCard/index.jsx
+++ src/components/cards/ParticipantCard/index.jsx
@@ -1,7 +1,21 @@
 import PropTypes from 'prop-types';
+import { Instagram, Linkedin, GitHub } from 'react-feather';
 import styles from './styles.module.css';
+import SecondaryIconButton from '../../buttons/SecondaryIconButton';
 
 export default function ParticipantCard({ participant }) {
+  const handleButtonClick = (link) => {
+    window.open(link);
+  };
+
+  const formatYearText = () => {
+    const { entry, leave } = participant.year;
+    if (leave !== null) {
+      return `${entry} - ${leave}`;
+    }
+    return `${entry} - até o momento`;
+  };
+
   return (
     <article className={styles.article}>
       <div className={styles.container}>
@@ -10,14 +24,37 @@ export default function ParticipantCard({ participant }) {
           <span className={styles.informations}>
             <p className={styles.name}>{participant.name}</p>
             <p className={styles.function}>{participant.function}</p>
+            <p className={styles.year}>{formatYearText()}</p>
           </span>
           <span className={styles.networks}>
-            {/* Botões das redes */}
+            {participant.networks && (
+              <>
+                {participant.networks.github && (
+                  <SecondaryIconButton
+                    Icon={GitHub}
+                    onClick={() => handleButtonClick(participant.networks.github)}
+                  />
+                )}
+                {participant.networks.linkedin && (
+                  <SecondaryIconButton
+                    Icon={Linkedin}
+                    onClick={() => handleButtonClick(participant.networks.linkedin)}
+                  />
+                )}
+                {participant.networks.instagram && (
+                  <SecondaryIconButton
+                    Icon={Instagram}
+                    onClick={() => handleButtonClick(participant.networks.instagram)}
+                  />
+                )}
+              </>
+            )}
           </span>
         </span>
         <span className={styles['informations-mobile']}>
           <p className={styles.name}>{participant.name}</p>
           <p className={styles.function}>{participant.function}</p>
+          <p className={styles.year}>{formatYearText()}</p>
         </span>
         <span>
           <p>{participant.bio}</p>
--- src/pages/o-projeto.jsx
+++ src/pages/o-projeto.jsx
@@ -1,5 +1,9 @@
 import Head from 'next/head';
 import PageTemplate from '../components/templates/PageTemplate';
+import SectionHeader from '../components/base/SectionHeader';
+import ParticipantCard from '../components/cards/ParticipantCard';
+
+import participantsData from '../data/project/participants.json';
 
 export default function TheProject() {
   return (
@@ -9,12 +13,22 @@ export default function TheProject() {
         <meta name="description" content="Um pouco do que inspirou esta iniciativa, quem nós somos e o que conseguimos construir" />
         <title>O Projeto</title>
       </Head>
-
       <PageTemplate
         title="O Projeto"
         description="Um pouco do que inspirou esta iniciativa, quem nós somos e o que conseguimos construir"
-      />
+      >
+        <SectionHeader
+          title="Membros"
+          description="Todos os membros que fazem e que fizeram parte do projeto"
+        />
+        <ParticipantList participants={participantsData.participants} />
+      </PageTemplate>
     </>
-
   );
+}
+
+function ParticipantList({ participants }) {
+  return participants.map((participant) => (
+    <ParticipantCard key={participant.order} participant={participant} />
+  ));
 }
