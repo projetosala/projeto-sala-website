0cca584ab1f9d6ab492d1513976b84870de3ca20
dhianapereira <dhianaslv@outlook.com>
2023-11-10 17:17:13 -0300

    refactor: Remove o botão de feeback

--- src/components/templates/PageTemplate/index.jsx
+++ src/components/templates/PageTemplate/index.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../base/Header';
 import Footer from '../../base/Footer';
-import FloatingButton from '../../buttons/FloatingButton';
-import FeedbackPopup from '../../cards/FeedbackPopup';
 
 export default function PageTemplate({ title, description, children }) {
-  const [isFeedbackVisible, setIsFeedbackVisible] = useState(false);
   return (
     <>
       <Header title={title} description={description} />
       {children}
-      <FloatingButton onClick={() => setIsFeedbackVisible(true)} />
-      {isFeedbackVisible ? <FeedbackPopup onClose={() => setIsFeedbackVisible(false)} /> : null}
       <Footer />
     </>
   );
